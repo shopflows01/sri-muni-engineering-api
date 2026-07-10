b9307b414b140b028b29d81e94e1d54074712210
Santhosh Kumar <sk18284176@gmail.com>
2026-07-10 19:59:45 +0530

    style: Reduce bottom spacer height to prevent page spillover

--- SriMuniEngineering-Api/SriMuniEngineering-Api/Features/Invoices/InvoicePdfGenerator.cs
+++ SriMuniEngineering-Api/SriMuniEngineering-Api/Features/Invoices/InvoicePdfGenerator.cs
@@ -220,7 +220,7 @@ public static class InvoicePdfGenerator
                 AddTaxRow("SGST", totalSgst);
                 AddTaxRow("IGST", totalIgst);
 
-                AddEmptyRow(120);
+                AddEmptyRow(50);
 
                 table.Cell().ColumnSpan(6).BorderTop(0.5f).BorderRight(0.5f).Padding(3).AlignRight().Text("Total").Bold().FontSize(11);
                 table.Cell().BorderTop(0.5f).Padding(3).AlignRight().Text($"₹ {invoice.GrandTotal:N2}").Bold().FontSize(11);
